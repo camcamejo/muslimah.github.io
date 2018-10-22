3730f70d0d51794d46d822608dbdc45f8098af9d
Camp Sci <info@camp-sci.com>
2018-10-21 17:33:30 -0700

    not sure either

---
+++ _site/src/views/Index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="wrapper">
+    <parallax class="page-header header-filter" :style="headerStyle">
+      <div class="md-layout">
+        <div class="md-layout-item">
+          <div class="image-wrapper">
+            <img :src="leaf4" alt="leaf4" class="leaf4" v-show="leafShow">
+            <img :src="leaf3" alt="leaf3" class="leaf3" v-show="leafShow">
+            <img :src="leaf2" alt="leaf2" class="leaf2" v-show="leafShow">
+            <img :src="leaf1" alt="leaf1" class="leaf1" v-show="leafShow">
+            <div class="brand">
+              <h1>Muslimah</h1>
+              <h3>TAGLINE HERE.</h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </parallax>
+    <div class="main main-raised">
+      <div class="section">
+        <div class="container">
+          <slideshow></slideshow>
+        </div>
+      </div>
+      <div class="section section-contacts">
+        <div class="container">
+          <div class="md-layout">
+            <div class="md-layout-item md-size-66 md-xsmall-size-100 mx-auto">
+              <h2 class="text-center title">SIGN UP</h2>
+              <h4 class="text-center description">Sign up to join the Muslimah mailing list to keep up to date with new blog posts, MCM's and events!</h4>
+              <form class="contact-form">
+                  <div class="md-layout">
+                      <div class="md-layout-item md-size-50">
+                        <md-field>
+                          <label>Your Name</label>
+                          <md-input v-model="name" type="text"></md-input>
+                        </md-field>
+                      </div>
+                      <div class="md-layout-item md-size-50">
+                        <md-field>
+                          <label>Your Email</label>
+                          <md-input v-model="email" type="email"></md-input>
+                        </md-field>
+                      </div>
+                  </div>
+                  <div class="md-layout">
+                    <div class="md-layout-item md-size-33 mx-auto text-center">
+                      <md-button class="md-success">SIGN ME UP!</md-button>
+                    </div>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import BasicElements from "./components/BasicElementsSection";
+import Slideshow from "./components/Slideshow";
+import { LoginCard } from "@/components";
+
+export default {
+  components: {
+    BasicElements,
+    Slideshow,
+    LoginCard
+  },
+  name: "index",
+  bodyClass: "index-page",
+  props: {
+    image: {
+      type: String,
+      default: require("@/assets/img/vue-mk-header.jpg")
+    },
+    leaf4: {
+      type: String,
+      default: require("@/assets/img/leaf4.png")
+    },
+    leaf3: {
+      type: String,
+      default: require("@/assets/img/leaf3.png")
+    },
+    leaf2: {
+      type: String,
+      default: require("@/assets/img/leaf2.png")
+    },
+    leaf1: {
+      type: String,
+      default: require("@/assets/img/leaf1.png")
+    },
+    signup: {
+      type: String,
+      default: require("@/assets/img/city.jpg")
+    },
+    landing: {
+      type: String,
+      default: require("@/assets/img/landing.jpg")
+    },
+    profile: {
+      type: String,
+      default: require("@/assets/img/profile.jpg")
+    }
+  },
+  data() {
+    return {
+      firstname: null,
+      email: null,
+      password: null,
+      leafShow: false
+    };
+  },
+  methods: {
+    leafActive() {
+      if (window.innerWidth < 768) {
+        this.leafShow = false;
+      } else {
+        this.leafShow = true;
+      }
+    }
+  },
+  computed: {
+    headerStyle() {
+      return {
+        backgroundImage: `url(${this.image})`
+      };
+    },
+    signupImage() {
+      return {
+        backgroundImage: `url(${this.signup})`
+      };
+    }
+  },
+  mounted() {
+    this.leafActive();
+    window.addEventListener("resize", this.leafActive);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.leafActive);
+  }
+};
+</script>
+<style lang="scss">
+.section-download {
+  .md-button + .md-button {
+    margin-left: 5px;
+  }
+}
+
+@media all and (min-width: 991px) {
+  .btn-container {
+    display: flex;
+  }
+}
+</style>
